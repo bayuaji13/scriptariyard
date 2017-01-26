f7841ec0124e69b6c676f4d1905f2f43df5397b8
Bayu Aji <bayu.xiii@gmail.com>
2017-01-26 14:53:57 +0700

    nomor 1 dah selesai

--- 1/1_tampilan_deret.php
+++ 1/1_tampilan_deret.php
@@ -1,12 +1,11 @@
 <?php 
-	$baris_prima = $baris_genap = $baris_fibbonaci = array();
+	$baris_prima = $baris_genap = $baris_fibbonaci = $counter = '';
 	if ($_POST != null){
 		$counter = $_POST['counter'];
+		$baris_fibbonaci = implode(',',get_n_fibbonaci($counter));
+		$baris_genap = implode(',',get_n_genap($counter));
+		$baris_prima = implode(',',get_n_prima($counter));
 	}
-
-	$baris_fibbonaci = get_n_fibbonaci(5);
-	print_r($baris_fibbonaci);
-
 
 	function get_n_fibbonaci($n)
 	{
@@ -17,6 +16,38 @@
 		while ($iter < $n){
 			$hasil[] = $hasil[$iter] + $hasil[$iter-1];
 			$iter = count($hasil)-1;
+		}
+		unset($hasil[0]);
+		return $hasil;
+	}
+
+	function get_n_genap($n)
+	{
+		$hasil = array();
+		$iter = count($hasil);
+		$i = 1;
+		while ($iter < $n) {
+			if ($i % 2 == 0){
+				$hasil[] = $i;
+			}
+			$iter = count($hasil);
+			$i++;
+		}
+		return $hasil;
+	}
+
+
+	function get_n_prima($n)
+	{
+		$hasil = array();
+		$iter = count($hasil);
+		$i = 1;
+		while ($iter < $n) {
+			if (is_prima($i)){
+				$hasil[] = $i;
+			}
+			$iter = count($hasil);
+			$i++;
 		}
 		return $hasil;
 	}
@@ -42,3 +73,10 @@
 	}
 
  ?>
+
+ <form action="#" method="post">
+ 	counter : <input type="number" name="counter" value="<?php echo $counter ?>"> <input type="submit" name="submit" value="Tampilkan Deret"> <br>
+	Deret genap : <input type="text" value="<?php echo $baris_genap ?>"><br>
+	Deret prima : <input type="text" value="<?php echo $baris_prima ?>"><br>
+	Deret fibbonaci : <input type="text" value="<?php echo $baris_fibbonaci ?>"><br>
+ </form>
